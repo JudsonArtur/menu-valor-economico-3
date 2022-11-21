800b388e1edee186e8049b01962f9917ccfbcdb8
judson-artur <judsoncapepa22@gmail.com>
2022-11-21 14:06:07 +0000

    fixing javascript click

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 
-$('.menu-item').on('click',function(){
+$('.menu-item').on('click',function(e){
+   e.stopPropagation();
    let submenu = $(this).data('submenu');
      if( $(`.${submenu}`).length == 0 || $(`.${submenu}`).hasClass('open')){
       $('.menu-shadow').removeClass('open');  
       $('.submenu').removeClass('open');
-      $('.submenu').find('ul').removeClass('open');
-       
+      $('.submenu').find('ul').removeClass('open');       
      }else{
         $('.submenu').addClass('open');
         $('.menu-shadow').addClass('open');
@@ -13,6 +13,13 @@ $('.menu-item').on('click',function(){
      }  
 });
 
-$('.menu-avatar').on('click',function(){
+$('.menu-avatar').on('click',function(e){
+   e.stopPropagation();
     $('.dropdown-menu').toggleClass('open');
 });
+$('body').on('click',function(){
+   $('.dropdown-menu').removeClass('open');
+   $('.menu-shadow').removeClass('open');  
+      $('.submenu').removeClass('open');
+      $('.submenu').find('ul').removeClass('open'); 
+})
