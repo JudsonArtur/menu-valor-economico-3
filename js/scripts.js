7300e45ed5728b5dc231915dfb7a0a9ac55b3629
judson-artur <judsoncapepa22@gmail.com>
2022-12-23 10:42:54 +0100

    alteraçoes concluidas

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,22 @@
 
-$('.menu-item').on('click',function(e){
-   e.stopPropagation();
-   $('.dropdown-menu').removeClass('open');
-   let submenu = $(this).data('submenu');
-     if( $(`.${submenu}`).length == 0 || $(`.${submenu}`).hasClass('open')){
-      $('.menu-shadow').removeClass('open');  
-      $('.submenu').removeClass('open');
-      $('.submenu').find('ul').removeClass('open');       
-     }else{
-        $('.submenu').addClass('open');       
-        $('.menu-shadow').addClass('open');
-        $('.submenu').find('ul').map(function(index){
-            $(`.submenu-${index}`).removeClass('open');
-        });
-        $(`.${submenu}`).addClass('open');
-     }  
-});
-
 $('.menu-avatar').on('click',function(e){
    e.stopPropagation();
     $('.dropdown-menu').toggleClass('open');
 });
 $('body').on('click',function(){
    $('.dropdown-menu').removeClass('open');
-   $('.menu-shadow').removeClass('open');  
-      $('.submenu').removeClass('open');
-      $('.submenu').find('ul').removeClass('open'); 
 });
 
 
 $('body').on('click','.newspaper_addon',function(){
-          let documentId = $(this).data('id');
-         
-          if(documentId === '' || documentId === undefined || documentId === null){ 
-            alert('Sem documento...');
-            return;
-         }
-          let config = { documentId: documentId, darkMode: true };
-          
-          CloudPDF(config, document.getElementById('viewer')).then((instance) => {});
-          $('.modal-test').css('display','block')
+    let documentId = $(this).data('id');
+    if(documentId === '' || documentId === undefined || documentId === null){ 
+        alert('Sem documento...');
+        return;
+    }
+    let config = { documentId: documentId, darkMode: true };
+    CloudPDF(config, document.getElementById('viewer')).then((instance) => {});
+    $('.modal-test').css('display','block')
 })
 $('body').on('click','.close-bar',function(){
     $('.modal-test').css('display','none')
@@ -76,7 +53,6 @@ $('.details-click').on('click',function(){
    $(this).parents('.ds-flex').siblings('.transaction-details').slideToggle(0);
 });
 
-
 $('.payment-ways--header').on('click',function(){
     $('.payment-gateway').find('input').prop('checked',false);
     $('.payment-transfer').find('input').prop('checked',false);
@@ -85,11 +61,11 @@ $('.payment-ways--header').on('click',function(){
     $(this).siblings('.payment-ways--body').addClass('open');
       
       if( $('#transfer').is(":checked")){
-         $('.payment-button').addClass('transfer');
-         $('.payment-button').text('Transferência Bancaria');
+        $('.pay-gateway').removeClass('open');
+        $('.pay-transfer').addClass('open');   
       }else{
-        $('.payment-button').removeClass('transfer');
-        $('.payment-button').text('Multicaixa Express');
+        $('.pay-transfer').removeClass('open');
+        $('.pay-gateway').addClass('open'); 
       }
     
 });
